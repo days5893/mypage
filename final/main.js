ad05a5ee825779a5049cb9153bdb86cb2719d833
days <30517006+days5893@users.noreply.github.com>
2021-02-07 21:32:51 +0900

    トグルメニュー実装

---
+++ final/main.js
@@ -0,0 +1,5 @@
+let menu = document.querySelector(".wrap");
+let btn = document.querySelector(".mobile-menu__btn");
+btn.addEventListener("click", function () {
+  menu.classList.toggle("menu-open");
+});
